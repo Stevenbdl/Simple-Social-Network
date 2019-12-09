0ebc88ae09c151f65069c77308bab8b0dcee0a39
Stevenbdl <51344776+Stevenbdl@users.noreply.github.com>
2019-12-09 19:28:39 -0400

    Update connection.py

--- dbconnection/connection.py
+++ dbconnection/connection.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 db = mysql.connector.connect(
     host='localhost',
-    user='steven',
-    password='HatakeKakashi8294403049&',
-    database='alumnos'
+    user='youruser',#put your name user mysql 
+    password='<your password>',#put your password user mysql 
+    database='<your database name>'#put your database here
 )
 
 cursor = db.cursor()
